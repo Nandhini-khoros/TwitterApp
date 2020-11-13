380e788404177853325f62cdf19af8afe5bdce26
Nandhini-khoros <snandhini499@gmail.com>
2020-11-13 09:11:23 +0530

    services and dao

---
+++ src/main/java/com/example/Twitter/dao/GetPost.java
@@ -0,0 +1,34 @@
+package com.example.Twitter.dao;
+
+import com.example.Twitter.dao.TwitterServiceInterface;
+import org.springframework.stereotype.Service;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GetPost implements TwitterServiceInterface {
+    public List<String> getTimeLine(Twitter twitter) throws TwitterException {
+        List<String> a = new ArrayList<>();
+        List<Status> s = twitter.getHomeTimeline();
+        for(Status sa : s){
+            a.add(sa.getText());
+        }
+        return a;
+    }
+    public String postMsg(Twitter twitter, String postMessage)
+    {
+        try {
+
+            Status status = twitter.updateStatus(postMessage);
+            return "Post Tweeted";
+        }
+        catch (TwitterException e)
+        {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+}
---
+++ src/main/java/com/example/Twitter/dao/TwitterDao.java
@@ -0,0 +1,18 @@
+package com.example.Twitter.dao;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.List;
+
+public class TwitterDao {
+
+    TwitterServiceInterface getPost =  new GetPost();
+    public List<String> getTimeLineMessage(Twitter twitter) throws TwitterException {
+        return getPost.getTimeLine(twitter);
+    }
+    public String postMessage(Twitter twitter,String postMessage)
+    {
+        return getPost.postMsg(twitter,postMessage);
+    }
+}
