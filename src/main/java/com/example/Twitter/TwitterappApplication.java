62bbbd41660569db06a0f3ec54d8a95b44709f41
Nandhini-khoros <snandhini499@gmail.com>
2020-12-05 13:27:28 +0530

    Final

--- src/main/java/com/example/Twitter/TwitterappApplication.java
+++ src/main/java/com/example/Twitter/TwitterappApplication.java
@@ -1,19 +1,10 @@
 package com.example.Twitter;
-import com.example.Twitter.controller.TwitterController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TwitterappApplication  {
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterappApplication.class, args);
 	}
-
-
-
 }
--- src/main/java/com/example/Twitter/controller/TwitterController.java
+++ src/main/java/com/example/Twitter/controller/TwitterController.java
@@ -1,14 +1,11 @@
 package com.example.Twitter.controller;
 import com.example.Twitter.service.TwitterService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
-
 import java.util.List;
 
 @RestController
@@ -24,8 +21,6 @@ public class TwitterController {
     @Value("${demo.accessTokenSecret}")
     String accessTokenSecret;
 
-
-
     public Twitter getTwitterInstance()
     {
         ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -39,10 +34,7 @@ public class TwitterController {
         Twitter twitter = twitterFactory.getInstance();
         return twitter;
     }
-
     TwitterService twitterService =  new TwitterService();
-
-
     @GetMapping(value = "/gettimeline")
     public List<String> getFeed() throws TwitterException
     {
@@ -53,6 +45,5 @@ public class TwitterController {
     public String postTweet(@PathVariable(value = "postMessage") String postMessage) {
         Twitter twitter = getTwitterInstance();
         return twitterService.postMessage(twitter,postMessage);
-
     }
 }
